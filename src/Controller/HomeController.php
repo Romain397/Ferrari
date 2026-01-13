bb3e14f97b14e484bb5ba0ee5f100a5c7d046eaa
Romain Mazille <mazilleromain5@gmail.com>
2026-01-13 15:03:04 +0100

    Mise en forme projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,64 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function home(): Response
     {
+        // Rendu de la page Twig
         return $this->render('home/home.html.twig');
-    }
-
-    #[Route('/about', name: 'about')]
-    public function about(): Response
-    {
-        return $this->render('home/about.html.twig');
-    }
-
-    #[Route('/hello/{name}', name: 'hello')]
-    public function hello(string $name): Response
-    {
-        return $this->render('home/hello.html.twig', [
-            'name' => ucfirst($name)
-        ]);
-    }
-
-    #[Route('/random', name: 'random')]
-    public function random(): Response
-    {
-        $quotes = [
-            "Symfony, c’est propre.",
-            "Le backend, c’est la base.",
-            "Twig simplifie la vie.",
-            "Coder, c’est créer."
-        ];
-
-        $quote = $quotes[array_rand($quotes)];
-
-        return $this->render('home/random.html.twig', [
-            'quote' => $quote
-        ]);
-    }
-
-    #[Route('/api/random', name: 'api_random')]
-    public function apiRandom(): JsonResponse
-    {
-        $quotes = ["A", "B", "C", "D"];
-        return new JsonResponse([
-            'quote' => $quotes[array_rand($quotes)]
-        ]);
-    }
-
-
-    #[Route('/redirect', name: 'redirect')]
-    public function redirectToRandom(): RedirectResponse
-    {
-        return $this->redirectToRoute('random');
     }
 }
