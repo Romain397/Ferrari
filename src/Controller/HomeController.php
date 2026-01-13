20b6a8703cf299f300c4b7e9a0dcd96c9a4b0078
Romain Mazille <mazilleromain5@gmail.com>
2026-01-13 15:18:43 +0100

    essai pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,41 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function home(): Response
     {
-        // Rendu de la page Twig
-        return $this->render('home/home.html.twig');
+        $cars = [
+            'Ferrari 499P',
+            'Ferrari 488 GTE',
+            'Ferrari 458 Italia',
+            'Ferrari FXX-K'
+        ];
+
+        return $this->render('home/home.html.twig', [
+            'cars' => $cars
+        ]);
+    }
+
+    #[Route('/sport-auto', name: 'sport_auto')]
+    public function sportAuto(): Response
+    {
+        $palmares = [
+            ['year' => 2023, 'title' => 'Victoire aux 24 Heures du Mans'],
+            ['year' => 2024, 'title' => 'Championnat WEC Hypercar'],
+            ['year' => 2025, 'title' => 'Double podium WEC']
+        ];
+
+        return $this->render('sport_auto/index.html.twig', [
+            'palmares' => $palmares
+        ]);
+    }
+
+    #[Route('/store', name: 'store')]
+    public function store(): Response
+    {
+        return $this->render('store/index.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig');
     }
 }
