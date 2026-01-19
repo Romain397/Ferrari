3eb565e9149dde24c5770ecb90a2a9bb53e9a289
Romain Mazille <mazilleromain5@gmail.com>
2026-01-19 10:08:43 +0100

    push

--- src/Controller/CarArticleController.php
+++ src/Controller/CarArticleController.php
@@ -14,16 +14,16 @@ class CarArticleController extends AbstractController
     #[Route('/admin/car-article/new', name: 'car_article_new')]
     public function new(Request $request): Response
     {
-        // 1️⃣ Instancier l'entité
+        // Instancier l'entité
         $carArticle = new CarArticle();
 
-        // 2️⃣ Créer le formulaire à partir du Type
+        // Créer le formulaire à partir du Type
         $form = $this->createForm(CarArticleType::class, $carArticle);
 
-        // 3️⃣ Ecouter la requête
+        // Ecouter la requête
         $form->handleRequest($request);
 
-        // 4️⃣ Vérifier si le formulaire est soumis et valide
+        // Vérifier si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
             // Debug : vérifier que l'objet est bien hydraté
             dump($carArticle); // Symfony Profiler affichera toutes les données
@@ -33,7 +33,7 @@ class CarArticleController extends AbstractController
 
         }
 
-        // 5️⃣ Rendu du template
+        // Rendu du template
         return $this->render('admin/car_article_new.html.twig', [
             'form' => $form->createView(),
         ]);
